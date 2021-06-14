b369f5245fb364e8c03599656ef19960513e8c9e
aleksandr8385 <sashasergienko8385@gmail.com>
2021-06-14 12:45:34 +0300

    получили курс валют из openexchangerates

--- aristos86/funcs.php
+++ aristos86/funcs.php
@@ -8,3 +8,29 @@ function dd($value)
     print_r($value);
     echo "</pre>";
 }
+
+/**получение курса валют базовая доллар */
+
+function getCurrency() {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://openexchangerates.org/api/latest.json?app_id=3eebbd52b9e34d389ebda9cee6b24b33',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    $response = json_decode($response);
+
+    curl_close($curl);
+    return $response->rates;
+}
