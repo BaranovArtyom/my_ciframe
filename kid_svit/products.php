78a2593d4d290e04a3d1af5828d0e2782696b83e
aleksandr8385 <sashasergienko8385@gmail.com>
2021-06-24 11:10:10 +0300

    add comm

--- kid_svit/products.php
+++ kid_svit/products.php
@@ -3,38 +3,30 @@ ini_set('display_errors', 'on');
 require_once 'funcs.php';
 require_once 'config.php';
 
-$logger = __DIR__.'/ci_log.log'; // создание лога и директории
+$logger = __DIR__.'/ci_log.log';                                // создание лога и директории
 $size_logger = filesize($logger);
-if ( $size_logger>11462000 ) file_put_contents($logger, ''); // 11mb , проверка на размер лога если более 11mb очистка
+if ( $size_logger>11462000 ) file_put_contents($logger, '');    // 11mb , проверка на размер лога если более 11mb очистка
 
-$getProduct = getProduct($KIDD_USER, $KIDD_PASSWORD);                     // получение товаров из ссылки
-// file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db)."\n",FILE_APPEND);
-// var_dump($getProduct);exit;
-$products = new SimpleXMLElement($getProduct);  // 
+$getProduct = getProduct($KIDD_USER, $KIDD_PASSWORD);           // получение товаров из ссылки
 
-// file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db)."\n",FILE_APPEND);
-foreach ($products as $product) {
-    // dd($product->Артикул);
-    // if (!$check=@mysqli_fetch_row(@mysqli_query($db,"SELECT `meta_prod` FROM `products`  WHERE `meta_prod`= '$meta_prod'"))[0]){
-    //     /**заполнение таблицы в retail_demand в бд */
-    //     $insertProd = mysqli_query($db,"INSERT INTO `products` (`id`, `meta_prod`, `name_prod`,`type_prod`,`sum_prod`,`href_product`) 
-    //     VALUES (NULL, '{$meta_prod}','{$name_prod}','{$type_prod}','{$sum_prod}','{$href_product}') ");
-    //     file_put_contents('logger.log',date('Y-m-d H:i:s').'  создания insertProd - '.$insertProd."\n",FILE_APPEND);
-    //     dd($insertProd);
-    //     echo "insert".$name_prod;
-    // }else {
-    //     $updateProd = mysqli_query($db,"UPDATE `products` SET `sum_prod` = '$sum_prod' WHERE `meta_prod`= '$meta_prod'");
-    //     file_put_contents('logger.log',date('Y-m-d H:i:s').'  update - '.$insertProd."\n",FILE_APPEND);
-    //     dd($updateProd);
-    //     echo "update".$name_prod;
-    // }
+$products = new SimpleXMLElement($getProduct);                  // 
 
-    dd($product);
-    // $prod['id'] = $i++;
-    $prod['barcode'] = $product->Штрихкод;
-    // echo $prod['barcode'];
-    $pr[] = $prod; 
-}
-
-// dd($pr);
-
+    foreach ($products as $product) {                           // проверка на существование в таблице
+        if (!$check=mysqli_fetch_row(mysqli_query($db,"SELECT `artikul` FROM `products`  WHERE `artikul`= '{$product->Артикул}'"))[0]){
+            
+        /**заполнение таблицы в products в бд */
+            $insertProd = mysqli_query($db,"INSERT INTO `products` (`id`, `artikul`) 
+            VALUES (NULL, '{$product->Артикул}') ");
+                if (mysqli_error($db)) {                        // проверка на  ошибку в запросе mysql записью в лог
+                    file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db)."\n",FILE_APPEND);
+                }
+            echo "insert".$product->Артикул;
+            dd($product->Артикул);
+        }else {
+            $updateProd = mysqli_query($db,"UPDATE `product` SET `artikul` = '{$product->Артикул}' WHERE `artikul`= '{$product->Артикул}'");
+            if (mysqli_error($db)) {                            // проверка на  ошибку в запросе mysql записью в лог
+                file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db)."\n",FILE_APPEND);
+            }
+            echo "update".$product->Артикул."<br>";
+        }
+    }
