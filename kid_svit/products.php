02b668b590c611539fdf50f55419c5bfedf7bcdb
aleksandr8385 <sashasergienko8385@gmail.com>
2021-06-24 17:08:20 +0300

    commit

--- kid_svit/products.php
+++ kid_svit/products.php
@@ -8,7 +8,7 @@ $size_logger = filesize($logger);
 if ( $size_logger>5462000 ) file_put_contents($logger, '');    // 5mb , проверка на размер лога если более 11mb очистка
 
 $getProduct = getProduct($KIDD_USER, $KIDD_PASSWORD);           // получение товаров из ссылки
-
+// dd($getProduct);exit;
 $products = new SimpleXMLElement($getProduct);                  // 
 
     foreach ($products as $product) {                           // проверка на существование в таблице
@@ -21,16 +21,19 @@ $products = new SimpleXMLElement($getProduct);                  //
         $rekomenden_year = addslashes($product->Рекомендация_по_возрасту_от);
         $name_N1 = addslashes($product->Наименование_Н1); 
         $brand = addslashes($product->Бренд);
+        $proizvoditel = addslashes($product->Производитель);
+        $href_image = addslashes($product->Путь_к_файлу_с_изображением_FTP);
         // dd($rekomenden_year);
         if (!$check=mysqli_fetch_row(mysqli_query($db,"SELECT `name_product` FROM `products`  WHERE `artikul`= '{$product->Артикул}'"))[0]){
             
         /**заполнение таблицы в products в бд */
 
             $insertProd = mysqli_query($db,"INSERT INTO `products` (`id`,`artikul`,`name_product`,`price`,`quantity`,`shelf_life`,`power_need`,`batteries`,`material`,`color`,`komplekt_in`,`made_in`,`rekomenden_year`,`play_to`,
-            `sex`,`status_product`,`type_individual_pack`,`code`,`name_N1`,`brand`) 
+            `sex`,`status_product`,`type_individual_pack`,`code`,`name_N1`,`brand`,`proizvoditel`,`sub_category`,`video`,`href_site`,`href_image`) 
             VALUES (NULL, '{$product->Артикул}','{$name_product}','{$product->Цена}','{$product->КоличествоОстаток}','{$product->Срок_годности_мес}','{$power_need}','{$product->Батарейки_входят_в_комплект}',
             '{$material}','{$product->Цвет}','{$komplekt_in}','{$made_in}','{$rekomenden_year}','{$product->Интересно_играть_в_возрасте_до}','{$product->Пол}','{$product->Статус}',
-            '{$product->Тип_индивидуальной_упаковки}','{$product->Код_УКТВЭД}','{$name_N1}','{$brand}'  ) ");
+            '{$product->Тип_индивидуальной_упаковки}','{$product->Код_УКТВЭД}','{$name_N1}','{$brand}','{$proizvoditel}','{$product->Подкатегория}','{$product->Видеообзор}','{$product->Ссылка_на_сайт}',
+            '{$href_image}') ");
                 if (mysqli_error($db)) {                        // проверка на  ошибку в запросе mysql записью в лог
                     file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
                 }
@@ -40,7 +43,8 @@ $products = new SimpleXMLElement($getProduct);                  //
             `shelf_life`='{$product->Срок_годности_мес}',`power_need`='{$power_need}',`batteries`='{$product->Батарейки_входят_в_комплект}',`material`='{$material}',
             `color`='{$product->Цвет}',`komplekt_in`='{$komplekt_in}',`made_in`='{$made_in}',`rekomenden_year`= '{$rekomenden_year}',`play_to`='{$product->Интересно_играть_в_возрасте_до}',
             `sex`='{$product->Пол}',`status_product`='{$product->Статус}',`type_individual_pack`='{$product->Тип_индивидуальной_упаковки}',`code`='{$product->Код_УКТВЭД}',
-            `name_N1`='{$name_N1}',`brand`='{$brand}' WHERE `artikul`= '{$product->Артикул}'");
+            `name_N1`='{$name_N1}',`brand`='{$brand}',`proizvoditel`='{$proizvoditel}',`sub_category`='{$product->Подкатегория}',`video`='{$product->Видеообзор}',
+            `href_site`='{$product->Ссылка_на_сайт}',`href_image`='{$href_image}' WHERE `artikul`= '{$product->Артикул}'");
             if (mysqli_error($db)) {                            // проверка на  ошибку в запросе mysql записью в лог
                 file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
             }
