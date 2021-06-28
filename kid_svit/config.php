6815fc459dd98f382601607e7a0991ffc01e65e3
aleksandr8385 <38703426+aleksandr8385@users.noreply.github.com>
2021-06-28 22:54:42 +0300

    Update config.php

--- kid_svit/config.php
+++ kid_svit/config.php
@@ -1,14 +1,48 @@
 <?php
 
-/**для соединения с базой */
+<?php
+require_once 'funcs.php';
+/**соединения с базой */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'sasha');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'kid_svit');
+define('DB_PASSWORD', 'пароль');
+define('DB_NAME', 'c_integr');
 
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Ошибка " . mysqli_error($db));
 mysqli_set_charset($db, 'utf8');
 
 /**пароли для 'https://bot.kiddisvit.ua/KiddisvitServices/hs/ImportDataProductsFile/?format=xml' */
-$KIDD_USER ='IamClient';
-$KIDD_PASSWORD ='BvgjhnAfqkjd@2020';
+$NAME_SITE = 'kiddisvit.ua';
+
+$config = array();
+$conf['password'] ='BvgjhnAfqkjd@2020';
+$conf['user'] = 'IamClient';
+
+$config = $conf;
+
+foreach ($config as $key=>$data) {
+    $check_config = '';
+    // dd($data);
+    $check_config= mysqli_query($db,"SELECT `name` FROM `ci_config`  WHERE `value`='{$data}' "); //проверка на существование конфига
+    $check_key = mysqli_query($db,"SELECT `key` FROM `ci_config`  WHERE `key`='{$key}' ");       // проверка на значение конфига кеу существовал если нет добавляем иначе обновляем
+    // dd($key);
+    // dd($check_config);
+    if($check_config->num_rows==0 and $check_key->num_rows==0 ){
+           
+                $insert_ci_config = mysqli_query($db,"INSERT INTO `ci_config` (`id`,`name`,`key`,`value`,`default`,`type`) 
+                VALUES (NULL, '{$NAME_SITE}','{$key}','{$data}',NULL,NULL) ");
+
+            
+            
+            if (mysqli_error($db)) {                        // проверка на  ошибку в запросе mysql записью в лог
+                file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
+            }
+    }else {
+            $update_ci_config = mysqli_query($db,"UPDATE `ci_config` SET `name` = '{$NAME_SITE}',`key` = '{$key}',`value`= '{$data}',`default`= NULL,`type`= NULL 
+                                            WHERE `key`= '{$key}'");
+            if (mysqli_error($db)) {                            // проверка на  ошибку в запросе mysql записью в лог
+                file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
+            }
+    }
+    
+}
