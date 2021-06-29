f7fe587ce2333b320f960c206c1e7afe86f8e30a
aleksandr8385 <sashasergienko8385@gmail.com>
2021-06-29 16:54:47 +0300

    add comm

--- kid_svit/goods_meta.php
+++ kid_svit/goods_meta.php
@@ -1,39 +1,70 @@
 <?php 
+
 ini_set('display_errors', 'on');
 require_once 'funcs.php';
 require_once 'config.php';
 
-$logger = __DIR__.'/ci_log.log';                                    // создание лога и директории
+$current_time = date("y-m-d h:i:s");
+
+$logger = __DIR__.'/ci_log.log';                                // создание лога и директории
 $size_logger = filesize($logger);
-    if ( $size_logger>5462000 ) file_put_contents($logger, '');    // 5mb , проверка на размер лога если более 11mb очистка
+if ( $size_logger>5462000 ) file_put_contents($logger, '');    // 5mb , проверка на размер лога если более 11mb очистка
 
-$getProduct = getProduct($conf['user'], $conf['password']);        // получение товаров из ссылки
+$getProduct = getProduct($conf['user'], $conf['password']);           // получение товаров из ссылки
+// dd($getProduct);exit;
+$products = new SimpleXMLElement($getProduct);                  // 
+/**получение товаров из таблицы ci_kiddsvit_goods */
 
-$products = new SimpleXMLElement($getProduct);                  
+// dd($getGoods['name']);
 
-    foreach ($products as $product) {                           // проверка на существование в таблице
+// exit;
 
-        /**получение id товаров из таблицы ci_kiddsvit_goods */
-        $getGoodId = mysqli_fetch_assoc(mysqli_query($db,"SELECT id FROM `ci_kiddsvit_goods` WHERE `sku`='{$sku}'"));
-        
-        foreach ($product as $key=>$attr) {
-            $attr = addslashes($attr); ;
-                if (!$check=mysqli_fetch_row(mysqli_query($db,"SELECT `meta_key` FROM `ci_kiddsvit_goods_meta`  WHERE `good_id`= '{$getGoodId['id']}'"))[0]){
+foreach ($products as $product) {                           // проверка на существование в таблице
+    // dd($product);
+    // dd(count($product));
+    $name_product = addslashes($product->НаименованиеПолное); // экранирование имени
+    // $power_need = addslashes($product->Питание);
+    // $material = addslashes($product->Материал);
+    // $komplekt_in = addslashes($product->В_комплект_входит);
+    // $made_in = addslashes($product->Страна_происхождения);
+    // $rekomenden_year = addslashes($product->Рекомендация_по_возрасту_от);
+    // $name_N1 = addslashes($product->Наименование_Н1); 
+    // $brand = addslashes($product->Бренд);
+    // $proizvoditel = addslashes($product->Производитель);
+    // $href_image = addslashes($product->Путь_к_файлу_с_изображением_FTP);
+    // $descption = addslashes($product->Описание);
+    $sku = addslashes($product->Артикул);
 
-                /**заполнение таблицы в ci_kiddsvit_goods в бд */
-                $insertProd = mysqli_query($db,"INSERT INTO `ci_kiddsvit_goods_meta` (`id`,`id_goods`,`meta_key`,`meta_value`) 
-                                            VALUES (NULL,'{$getGoodId['id']}','{$key}','{$attr}') ");
+    $getGoodId = mysqli_fetch_assoc(mysqli_query($db,"SELECT id FROM `ci_kiddsvit_goods` WHERE `sku`='{$sku}'"));
+    // dd($getGoodId);exit;
+    foreach ($product as $key=>$attr) {
+        // dd($key);
+        // echo $key;
+        $attr = addslashes($attr); ;
+        // dd($attr);
+        if (!$check=mysqli_fetch_row(mysqli_query($db,"SELECT `meta_key` FROM `ci_kiddsvit_goods_meta`  WHERE `good_id`= '{$getGoodId['id']}'"))[0]){
+
+            /**заполнение таблицы в ci_kiddsvit_goods в бд */
+            $insertProd = mysqli_query($db,"INSERT INTO `ci_kiddsvit_goods_meta` (`id`,`id_goods`,`meta_key`,`meta_value`) 
+                VALUES (NULL,'{$getGoodId['id']}','{$key}','{$attr}') ");
                     if (mysqli_error($db)) {                        // проверка на  ошибку в запросе mysql записью в лог
                         file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
                     }
                 echo "insert".$product->Артикул."<br>";
-                }else {
-                    $updateProd = mysqli_query($db,"UPDATE `ci_kiddsvit_goods_meta` SET `id_goods`='{$getGoodId['id']}',`meta_key`='{$key}',`meta_value`= '{$attr}'
-                                              WHERE `good_id`= '{$getGoodId['id']}'");
-                    if (mysqli_error($db)) {                            // проверка на  ошибку в запросе mysql записью в лог
-                        file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
-                    }
-                echo "update".$product->Артикул."<br>";
+            }else {
+                $updateProd = mysqli_query($db,"UPDATE `ci_kiddsvit_goods_meta` SET `id_goods`='{$getGoodId['id']}',`meta_key`='{$key}',`meta_value`= '{$attr}'
+                WHERE `good_id`= '{$getGoodId['id']}'");
+                if (mysqli_error($db)) {                            // проверка на  ошибку в запросе mysql записью в лог
+                    file_put_contents('ci_log.log',date('Y-m-d H:i:s').'  ошибка в бд - '.mysqli_error($db).'  '.$name_product."\n",FILE_APPEND);
                 }
-        }
+                echo "update".$product->Артикул."<br>";
+            }
     }
+    // dd($getGood);
+    // echo $getGood['id']; 
+    // exit;
+
+        
+
+
+}
