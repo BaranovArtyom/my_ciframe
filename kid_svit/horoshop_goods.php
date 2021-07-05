2585f2d32b43aaffb13919306355e6921bb47040
aleksandr8385 <sashasergienko8385@gmail.com>
2021-07-05 17:03:00 +0300

    add

--- kid_svit/horoshop_goods.php
+++ kid_svit/horoshop_goods.php
@@ -1,12 +1,62 @@
 <?php 
-
 ini_set('display_errors', 'on');
 require_once 'funcs.php';
 require_once 'config.php';
 
 /**артикулы товаров */
-$sku['article'] = ["leather12baltic","ll38pinegreen"];
+$sku['article'] = ["lesublack","ker140"];
 
-/**получение продуктов по id */
+/**получение продуктов по sku */
 $getGoods = getGoods($sku,$config_horoshop['token']);
-dd($getGoods);
+// dd($getGoods);
+$page = 0; $limit = 500;
+$offset = $page * $limit;
+// $getAllGoods = getAllGoods($offset ,$config_horoshop['token'],$limit);
+// if (!$getAllGoods) {
+//     echo "empty";
+// }
+// dd($getAllGoods);exit;
+
+/**перебор всех  продаж розницы*/
+$allGoods = array();
+$getGoods = array();
+
+$isProduct = true;
+while ($isProduct == true) {
+    // $getGoods = array();
+    $offset = $page * $limit;
+    $getGoods = getAllGoods($offset ,$config_horoshop['token'],$limit);
+    dd($offset);
+    dd($getGoods);
+
+    // $allGoods[] = $getGoods;
+
+    if ($offset == 16500) {
+    //     echo "empty";
+        $isProduct = false;
+    }
+    // $allGoods = array_merge($allGoods, $getGoods);
+    $page++;
+}
+// dd($allGoods);
+exit;
+
+foreach ($getGoods as $good) {
+   
+    
+    // if ($check=@mysqli_fetch_row(mysqli_query($db,"SELECT * FROM `ci_kiddsvit_goods_meta`  WHERE `meta_key` = 'Артикул' and `meta_value`='{$good->article}'"))[0]) { 
+    //     // $product['name'] = $good->title->ru;
+    //     // echo "SELECT `id_goods` FROM `ci_kiddsvit_goods_meta`  WHERE `meta_key` = 'Артикул' and `meta_value`= '{$article}'";exit;
+    //     $product['article'] = $good->article;
+    //     $product['price_old'] = 120;
+        
+    //     $body['products'][] = $product;
+    //     // dd($body);
+    //     $UpdateGood = UpdateGood($body, $config_horoshop['token']);
+    //     dd($UpdateGood);
+    // }else {
+    //     echo "надо добавить"."</br>";
+    //     dd($check);
+    // }
+    dd($good);
+}
